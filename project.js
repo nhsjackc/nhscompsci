d7d8e7f972974f4c5496096af934ba8ec7e033ab
nhsjackc <82103596+nhsjackc@users.noreply.github.com>
2021-04-15 13:46:00 -0400

    Update project.js

--- project.js
+++ project.js
@@ -1,5 +1,17 @@
 onEvent("r1c1", "click", function( ) {
-  setProperty("r1c1", "background-color", "red");
-  setProperty("r1c2", "background-color", "red");
-  setProperty("r2c1", "background-color", "red");
+  if (getProperty("r1c1", "background-color") == "blue") {
+    setProperty("r1c1", "background-color", "red");
+  } else {
+    setProperty("r1c1", "background-color", "blue");
+  }
+  if (getProperty("r1c2", "background-color") == "blue") {
+    setProperty("r1c2", "background-color", "red");
+  } else {
+    setProperty("r1c2", "background-color", "blue");
+  }
+  if (getProperty("r2c1", "background-color") == "blue") {
+    setProperty("r2c1", "background-color", "red");
+  } else {
+    setProperty("r2c1", "background-color", "blue");
+  }
 });
