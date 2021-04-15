dd021fe038345f1f61e9842c68cf05dcd60fb73e
nhsjackc <82103596+nhsjackc@users.noreply.github.com>
2021-04-15 13:35:55 -0400

    Create project.js

---
+++ project.js
@@ -0,0 +1,5 @@
+onEvent("r1c1", "click", function( ) {
+  setProperty("r1c1", "background-color", "red");
+  setProperty("r1c2", "background-color", "red");
+  setProperty("r2c1", "background-color", "red");
+});
