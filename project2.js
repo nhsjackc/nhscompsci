46b04b47437cd873ab0c128083f92f37d1ecd925
nhsjackc <82103596+nhsjackc@users.noreply.github.com>
2021-06-03 12:53:38 -0400

    Update project2.js

--- project2.js
+++ project2.js
@@ -1,1 +1,18 @@
-
+var clicks = 0;
+var upgrade = 1;
+var price = upgrade * (upgrade / 2) * 10;
+setProperty("counter", "text", clicks);
+setProperty("shop", "text", "upgrade: " + price);
+onEvent("button", "click", function( ) {
+  clicks = clicks + upgrade;
+  setProperty("counter", "text", clicks);
+});
+onEvent("upgrade", "click", function( ) {
+  if (clicks >= price) {
+    upgrade = upgrade * 2;
+    clicks = clicks - price;
+    setProperty("counter", "text", clicks);
+  }
+  price = upgrade * (upgrade / 2) * 10;
+  setProperty("shop", "text", "upgrade: " + price);
+});
